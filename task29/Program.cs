76616ec31dbfb4b3a1c1310e9c27ce3a67470b9e
Olga <supernatural64@mail.ru>
2022-08-23 12:55:13 +0400

    task29

---
+++ task29/Program.cs
@@ -0,0 +1,31 @@
+﻿//Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+//5 -> [1, 2, 5, 7, 19]
+//3 -> [6, 1, 33]
+
+int N = ReadInt("Введите число: ");
+
+int[] numbers = new int[N];
+FillArray(numbers);
+PrintArray(numbers);
+
+void FillArray(int[] array)
+{
+for(int i = 0; i < array.Length; i++)
+{
+array[i] = new Random().Next(0, 99);
+}
+}
+
+void PrintArray(int[] array)
+{
+for(int i = 0; i < array.Length; i++)
+{
+Console.Write(array[i] + " ");
+}
+}
+
+int ReadInt(string message)
+{
+Console.Write(message);
+return Convert.ToInt32(Console.ReadLine());
+}
